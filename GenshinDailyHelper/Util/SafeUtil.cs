39c51e17df017c15f158197acfaf8b0d9c1cc6bb
CathyCorellian <CathyCorellian@live.com>
2021-02-11 05:38:47 +0800

    Update SafeUtil.cs

--- GenshinDailyHelper/Util/SafeUtil.cs
+++ GenshinDailyHelper/Util/SafeUtil.cs
@@ -33,6 +33,14 @@ namespace GenshinDailyHelper.Util
             return (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / number;
         }
 
+        static char[] charList = {
+                'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                'A','B','C','D','E','F','G','H','I','J','K','L','M',
+                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                '0','1','2','3','4','5','6','7','8','9',
+            };
+
         /// <summary>
         /// 固定长度的随机字符串
         /// </summary>
@@ -40,13 +48,6 @@ namespace GenshinDailyHelper.Util
         /// <returns>随机串</returns>
         public static string GetRandString(int length)
         {
-            char[] charList = {
-                'a','b','c','d','e','f','g','h','i','j','k','l','m',
-                'n','o','p','q','r','s','t','u','v','w','x','y','z',
-                'A','B','C','D','E','F','G','H','I','J','K','L','M',
-                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                '0','1','2','3','4','5','6','7','8','9',
-            };
             char[] rev = new char[length];
             Random random = new Random();
             for (int i = 0; i < length; i++)
